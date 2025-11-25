f633d6ae999d689c2b0be06c25955cef82d01596
Alexander Lang <alex@cobot.me>
2025-11-25 07:59:50 +0000

    don't autocomplete names/emails

--- src/routes/users/new/+page.svelte
+++ src/routes/users/new/+page.svelte
@@ -25,6 +25,7 @@
 					type="text"
 					id="name"
 					name="name"
+					autocomplete="off"
 					required
 					class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
 				/>
@@ -36,6 +37,7 @@
 					type="email"
 					id="email"
 					name="email"
+					autocomplete="off"
 					required
 					class="w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
 				/>
